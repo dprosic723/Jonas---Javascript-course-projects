d4b8a1a46a12d795fa1afbd9c76d02e2968c66c9
dprosic723 <101458278+dprosic723@users.noreply.github.com>
2022-04-22 10:50:17 +0200

    Add functionality

--- Guess my number/script.js
+++ Guess my number/script.js
@@ -1,1 +1,41 @@
 'use strict';
+
+let randomNum = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+const currentNumBox = document.querySelector('.number');
+const againBtn = document.querySelector('.again');
+const inputBox = document.querySelector('.guess');
+const message = document.querySelector('.message');
+const scoreBox = document.querySelector('.score');
+const highScoreBox = document.querySelector('.highscore');
+
+const checkButton = document.querySelector('.check');
+checkButton.addEventListener('click', () => {
+  let inputNum = inputBox.value;
+  currentNumBox.textContent = inputNum;
+
+  if (inputNum != randomNum) {
+    score--;
+    scoreBox.textContent = score;
+    if (inputNum > randomNum) {
+      message.textContent = 'Too high!';
+    }
+    if (inputNum < randomNum) {
+      message.textContent = 'Too low!';
+    }
+  }
+  if (inputNum == randomNum) {
+    message.textContent = 'Correct!';
+    if (highScoreBox.textContent < score) highScoreBox.textContent = score;
+    score = 20;
+    randomNum = Math.trunc(Math.random() * 20) + 1;
+  }
+});
+
+againBtn.addEventListener('click', () => {
+  currentNumBox.textContent = '?';
+  randomNum = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  scoreBox.textContent = score;
+});
